36192a2a90749026a930507ce89265d6a6b51bea
slague <stephanielague@gmail.com>
2017-09-16 13:24:30 -0600

    Add lib and test directories
    *Write first test, requires arm to be valid

---
+++ lib/block_builder.rb
@@ -0,0 +1,5 @@
+class BlockBuilder
+
+  
+
+end
---
+++ test/block_builder_test.rb
@@ -0,0 +1,25 @@
+require 'minitest/autorun'
+require_relative '../lib/block_maker'
+require 'minitest/pride'
+
+
+class BlockBuilderTest < Minitest::Test
+
+  def test_block_maker_is_valid
+    blocker = BlockBuilder.new(3)
+    assert_instance_of BlockBuilder, blocker
+    assert_equal 3, blocker.arm.length
+    assert_equal ["1:", "2:", "3:"], blocker.arm
+  end
+
+# Design a command-line controller program for a robotic arm that takes commands that move blocks stacked in a series of slots. After each command, output the state of the slots, which each line of output corresponding to a slot and each X corresponding to a block.
+#
+# Commands:
+#
+# size [n] - Adjusts the number of slots, resizing if necessary. Program must start with this to be valid.
+# add [slot] - Adds a block to the specified slot.
+# mv [slot1] [slot2] - Moves a block from slot1 to slot2.
+# rm [slot] - Removes a block from the slot.
+# replay [n] - Replays the last n commands.
+# undo [n] - Undo the last n commands.
+# Your program should validate that the commands are syntactically valid before executing them.
